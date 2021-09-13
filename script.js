ea79f913600d3b079ddef981f4c59a1664e97f0a
Roosde <90309300+Roosdejong@users.noreply.github.com>
2021-09-13 07:43:27 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var x;
+function setup() {
+	createCanvas(500, 400);
+
+  x = 30;
+}
+
+
+function draw() {
+	background(225);
+
+  circle(x, 130, 20);
+  
+  x = x + 5;
+  
+  if(x > 300){
+    x = 0;
+  }
+
+}
+	
